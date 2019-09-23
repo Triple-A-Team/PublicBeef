6aa56c474e9b0c2d3ec7dae87e129ccc1e4004fc
alejandrogomez <alegomez1@me.com>
2019-09-23 12:17:04 -0400

    Made div for map, added code in the script.js file

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+// Initialize and add the map
+function initMap() {
+  // The location of Building Co
+  var park = {
+      lat: 25.723354,
+      lng: -80.321624
+  };
+  // var lake = {
+  //     lat: 25.719614,
+  //     lng: -80.369308
+  // };
+
+  // The map, centered at Tropical Park
+  var map = new google.maps.Map(
+      document.getElementById('map'), {
+          zoom: 11,
+          center: park
+      });
+  // The marker, positioned at park
+  var marker = new google.maps.Marker({
+      position: park,
+      map: map
+  });
+  //To make multiple markers
+  // var marker2 = new google.maps.Marker({
+  //     position: lake,
+  //     map: map
+  // });
+
+}
