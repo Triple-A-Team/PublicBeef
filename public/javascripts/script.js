968a5b5dc22f4e5060a9afa76314f9fdaac3dfb8
alejandrogomez <alegomez1@me.com>
2019-09-23 12:39:33 -0400

    Added radius for location, removed marker

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,26 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
+
+
+
+//Object of locations
+
+var locations={
+  building:{
+    center: {
+      lat: 25.766675,
+      lng: -80.196130  
+    }
+  }
+  }
+
 // Initialize and add the map
 function initMap() {
-  // The location of Building Co
-  var building = {
-      lat: 25.766675,
-      lng: -80.196130
 
-       
-  };
-  // var lake = {
-  //     lat: 25.719614,
-  //     lng: -80.369308
-  // };
-
-  // The map, centered at Tropical Park
   var map = new google.maps.Map(
       document.getElementById('map'), {
-          zoom: 11,
-          center: building
+          zoom: 17,
+          center: {lat: 25.766675, lng:-80.196130}
       });
+
+        // Construct the circle for each value in citymap.
+        // Note: We scale the area of the circle based on the population.
+        for (var city in locations) {
+          // Add the circle for this city to the map.
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: {lat: 25.766675, lng:-80.196130},
+            radius: 50 //Math.sqrt(citymap[city].population) * 100
+              
+          });
+        }
+
   // The marker, positioned at park
   var marker = new google.maps.Marker({
       position: building,
