6b15084334d00ebaa90abf3a2ec41e56998c3fa4
Andres Weber <andresmweber@gmail.com>
2019-09-23 22:56:02 -0400

    Added routes for user avatar stuff and added placeholders for the email/avatar.

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../../models/User')
 const { isLoggedIn } = require('../../middleware/auth')
 const router = express.Router()
+
+/** 
+ * Uploads an avatar file
+ */
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
 
 /** 
  * Get all users within a specific distance.
@@ -32,4 +49,76 @@ router.get('/search', (req, res, next) => {
         })
         .catch(err => next(err))
 })
+
+/** 
+ * Obtains a copy of the current logged in user model
+ * @example
+ * GET /api/users/me
+ */
+router.get(`/me`, async(req, res) => {
+    res.send(req.user)
+})
+
+/** 
+ * Edits fields of the currently logged in user
+ * @example
+ * PATCH /api/users/me 
+ */
+router.patch(`/me`, async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!' })
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+/** 
+ * Upload an avatar and save to the user document
+ * @example
+ * POST /api/users/me/avatar "avatar.jpg"
+ */
+router.post(`/me/avatar`, upload.single('avatar'), async(req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+/** 
+ * Get a user's avatar and send back the image
+ * @example
+ * GET /api/users/:id/avatar 
+ */
+router.get(`/:id/avatar`, async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar) throw new Error()
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
+/** 
+ * Delete the current user's avatar
+ * @example
+ * DELETE /api/users/me/avatar
+ */
+router.delete(`/me/avatar`, async(req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
 module.exports = router
