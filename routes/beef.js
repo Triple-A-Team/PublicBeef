b4b082d8758e492d5b3828efdaf1b756f359ea60
Adrian <adrianivsdk@gmail.com>
2019-09-24 19:05:51 -0400

    Not so much, just working on profile view

--- routes/beef.js
+++ routes/beef.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const User = require('../routes/api/user')
 const router = express.Router();
 
 /* GET home page */
+
 router.get('/beef', (req, res, next) => {
-    res.render('beef');
+    res.render('beef', {user: req.user});
 });
+
+// router.get('/api/users/me', (req,res,next)=>{
+   
+// })
 
 // router.get('/api-key', (req, res, next) => {
 //     res.json({
