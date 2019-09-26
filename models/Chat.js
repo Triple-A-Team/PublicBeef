4c3023bb470496d5e31965091f130d09af4c11b7
Andres Weber <andresmweber@gmail.com>
2019-09-25 23:39:40 -0400

    Added Chat Model.

---
+++ models/Chat.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const chatSchema = new Schema({
+    users: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    messages: [{
+        type: Schema.Types.ObjectId,
+        ref: "ChatMessage"
+    }]
+})
+
+function autopopulate(next) {
+    this.populate([
+        { path: 'messages' },
+        { path: 'users' }
+    ])
+    next();
+}
+
+chatSchema.pre('find', autopopulate);
+chatSchema.pre('findOne', autopopulate);
+
+const Chat = mongoose.model('Chat', chatSchema)
+
+module.exports = Chat
